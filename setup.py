458ac91c91691c302caf7b4789ba20d27a87ed37
Adrian MAURIN <adrian.maurin@gmail.com>
2016-04-26 11:01:56 +0200

    Bump version 0.3.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', encoding='utf-8') as f:
 
 setup(
     name='addic7ed',
-    version='0.3.2',
+    version='0.3.3',
 
     description=('Addic7ed scraper written in Python '
                  'to download subtitles (almost) automatically'),
